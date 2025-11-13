9d19c69927d0e3f437d48bea697aa45e0434cc2d
RandomKittenCat <75283280+RandomKittenCat@users.noreply.github.com>
2025-11-12 17:24:57 -0800

    pc

---
+++ src/WordMatch/WordMatch.java
@@ -0,0 +1,19 @@
+package WordMatch;
+
+public class WordMatch {
+    private String secret;
+
+    public WordMatch(String word){
+        /* implementation not shown */
+    }
+
+    public int scoreGuess(String guess){
+        int occurences = 0;
+        int lastIndex = 0;
+
+        while(lastIndex <= secret.length()){
+
+        }
+    }
+
+}
