9cadebf667b7a594041cb62b37352e675aea68ac
RandomKittenCat <75283280+RandomKittenCat@users.noreply.github.com>
2026-01-15 10:38:14 -0800

    bubblesort method

--- src/Notes/jan132026/BinarySearch.java
+++ src/Notes/jan132026/BinarySearch.java
@@ -5,7 +5,24 @@ public class BinarySearch {
 
         int[] arr = {10, 14, 17, 19, 22, 33, 65, 72, 88, 91};
 
+        bubbleSort(arr);
+        for(int i : arr){
+            System.out.print(i + " ");
+        }
         System.out.println(binSearch(arr,17));
+    }
+
+    // simple swap sort method
+    public static void bubbleSort(int[] a){
+        for(int i = 0; i < a.length-1; i++){
+            for(int j = 0; j < a.length-i-1; j++){
+                if(a[j] > a[j+1]){
+                    int temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1]=temp;
+                }
+            }
+        }
     }
 
     public static boolean binSearch(int[] a, int target){
