328f5c84d8ec882e0ca429d8eea920bd9e8cd092
Colin Dean <75283280+RandomKittenCat@users.noreply.github.com>
2026-03-23 22:26:26 -0700

    start of applecrate frq

---
+++ src/PieCreator/Apple.java
@@ -0,0 +1,21 @@
+package PieCreator;
+
+public class Apple {
+    /**
+     * Returns the weight of the apple, in pounds
+     */
+    public double getWeight(){
+        return 0.0;
+        // not shown
+    }
+
+    /**
+     * Returns true if the apple is rotten and false otherwise
+     */
+    public boolean isRotten(){
+        return false;
+        // not shown
+    }
+
+    // other constructors, instance variables. methods not shown
+}
---
+++ src/PieCreator/AppleCrate.java
@@ -0,0 +1,20 @@
+package PieCreator;
+
+public class AppleCrate {
+    private Apple[][] apples;
+
+    /**
+     * Returns the number of pies that can be made from the 2D array apples
+     * if each pie requires poundsPerPie pounds of apples. If an apple is
+     * rotten, it cannot be used in a pie, nor can any adjacent apple.
+     * Preconditions: apples has at least two rows and at least two columns.
+     *                  No elements in apples are null.
+     */
+    public int numberOfPies(double poundsPerPie){
+        for (int row = 0; row < apples.length; row++){
+            for (int col = 0; col < apples[row].length; col++){
+                
+            }
+        }
+    }
+}
