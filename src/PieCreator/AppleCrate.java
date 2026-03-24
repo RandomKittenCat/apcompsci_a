3985ccbf5f8df33a7ef6afcabe519d0b21a5d254
Colin Dean <75283280+RandomKittenCat@users.noreply.github.com>
2026-03-24 16:55:41 -0700

    apple crate frq

--- src/PieCreator/AppleCrate.java
+++ src/PieCreator/AppleCrate.java
@@ -11,10 +11,37 @@ public class AppleCrate {
      *                  No elements in apples are null.
      */
     public int numberOfPies(double poundsPerPie){
+        double usableWeight = 0.0;
+
         for (int row = 0; row < apples.length; row++){
             for (int col = 0; col < apples[row].length; col++){
-                
+                boolean isRuined = apples[row][col].isRotten();
+
+                // checking above
+                if (!isRuined && row > 0 && apples[row - 1][col].isRotten()) {
+                    isRuined = true;
+                }
+
+                // checking below
+                if (!isRuined && row < apples.length - 1 && apples[row + 1][col].isRotten()) {
+                    isRuined = true;
+                }
+
+                // checking left
+                if (!isRuined && col > 0 && apples[row][col - 1].isRotten()) {
+                    isRuined = true;
+                }
+
+                // checking right
+                if (!isRuined && col < apples[row].length - 1 && apples[row][col + 1].isRotten()) {
+                    isRuined = true;
+                }
+
+                if(!isRuined){
+                    usableWeight += apples[row][col].getWeight();
+                }
             }
         }
+        return (int) (usableWeight/poundsPerPie);
     }
 }
